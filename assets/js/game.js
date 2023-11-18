567d3ab2cbea1d078a4db9d4f3a8cccbf897417d
isdanryan <isdanryan@gmail.com>
2023-11-18 21:24:46 +0000

    Add random stars to game area

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,10 @@
 // game area variables
 let gamearea;
 let gameareaHeight = 300;
-let gameareaWidth = 800;
+let gameareaWidth = 700;
 let context;
 let gameover = false;
+let score = 0;
 
 
 // ufo variables
@@ -19,15 +20,35 @@ let ufo = {
     width: ufoWidth
 }
 
-
 // asteroid variables
 let asteroidArray = [];
-let asteroidX = 790;
+let asteroidX = 700;
 let asteroidY;
 let asteroidImage;
 let asteroidHeight = 50;
 let asteroidWidth = 50;
 let moveSpeed = -4; // set speed asteroids move accross screen right to left
+
+// stars for background
+
+let starArray = [];
+
+let star1Width = 10;
+let star1Height = 10;
+let star2Width = 20;
+let star2Height = 20;
+let star3Width = 30;
+let star3Height = 30;
+
+let starX = 700;
+let starY;
+
+let star1Image;
+let star2Image;
+let star3Image;
+
+let starSpeed = -2;
+
 
 //draw game area
 window.onload = function() {
@@ -47,8 +68,19 @@ window.onload = function() {
         context.drawImage(ufoImage, ufo.x, ufo.y, ufo.width, ufo.height);
     }
 
+    // draw background stars
+    star1Image = new Image();
+    star1Image.src = "./assets/img/star1.png";
+
+    star2Image = new Image();
+    star2Image.src = "./assets/img/star2.png";
+
+    star3Image = new Image();
+    star3Image.src = "./assets/img/star3.png";
+
     requestAnimationFrame(update);
-    setInterval(addAsteroid, 1000); // amount of time to place an asteroid omn canvas
+    setInterval(addAsteroid, 1000); // frequency asteroids are drawn
+    setInterval(addStar, 500); // frequency stars are drawn
 }
 
 document.addEventListener("keydown", function(event){
@@ -75,16 +107,22 @@ function update(){
         return;
     };
     context.clearRect(0, 0, gamearea.width, gamearea.height);
+    for (let s = 0; s < starArray.length; s++){
+        let star = starArray[s];
+        star.x += starSpeed;
+        context.drawImage(star.img, star.x, star.y, star.width, star.height);
+    }
     context.drawImage(ufoImage, ufo.x, ufo.y, ufo.width, ufo.height);
     for (let i = 0; i < asteroidArray.length; i++){
         let asteroid = asteroidArray[i];
         asteroid.x += moveSpeed;
-        context.drawImage(asteroidImage, asteroid.x, asteroid.y, asteroid.width, asteroid.height)
+        context.drawImage(asteroidImage, asteroid.x, asteroid.y, asteroid.width, asteroid.height);
         if (detectColision(ufo, asteroid)) {
             gameover = true;
             console.log("gameover");
         }
     }
+    
     
 }
 
@@ -111,7 +149,7 @@ function addAsteroid() {
 
     //create chance to spawn asteroid
     let asteroidChance = Math.random();
-    if (asteroidChance > 0.40) { // check chance isn't to often or not enough
+    if (asteroidChance > 0.30) { // check chance isn't to often or not enough
         //draw asteroid
         asteroidImage = new Image();
         asteroidImage.src = "./assets/img/asteroid-1.jpg";
@@ -129,5 +167,45 @@ function detectColision(a, b) {
     return a.x < b.x + b.width && //a's top left corner dosen't reach b's top right corner
             a.x + a.width > b.x && //a's top right corner passes b's top left corner
             a.y < b.y + b.height && //a's top left corner dosen't reach b's bottom left corner
-            a.y + a.height > b.y //a's bottom left corner passes b's top left corner
+            a.y + a.height > b.y //a's bottom right corner passes b's top left corner
+}
+
+function addStar() {
+    if (gameover) {
+        return;
+    };
+    let star = {
+        x: starX,
+        y: starY,
+        height: null,
+        width: null,
+        img: null
+    }
+
+    //create chance to spawn asteroid
+    let starChance = Math.random();
+    if (starChance > 0.80) { // check chance isn't to often or not enough
+        star.img = star3Image;
+        star.y = Math.floor(Math.random() * 270);
+        star.width = star3Width;
+        star.height = star3Height;
+        starArray.push(star); // add asteroid to array to track accross screen on redraws
+    } else if (starChance > 0.40) { // check chance isn't to often or not enough
+        star.img = star2Image
+        star.y = Math.floor(Math.random() * 270);
+        star.width = star2Width;
+        star.height = star2Height;
+        starArray.push(star); // add asteroid to array to track accross screen on redraws
+    } else if (starChance > 0.10) { // check chance isn't to often or not enough
+        star.img = star1Image;
+        star.y = Math.floor(Math.random() * 270);
+        star.width = star1Width;
+        star.height = star1Height;
+        starArray.push(star); // add asteroid to array to track accross screen on redraws
+    }
+
+    // remove asteroids from array once off screen to prevent taking too much memory over time
+    if (starArray.length > 20) {
+        starArray.shift();
+    }
 }
